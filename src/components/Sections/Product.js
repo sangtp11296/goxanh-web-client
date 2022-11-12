c0164108d92d8b5814068c23c888224473072ce4
Sang Tran Phuc <sang.tp.11296@gmail.com>
2022-11-13 00:48:33 +0700

    up

--- src/components/Sections/Product.js
+++ src/components/Sections/Product.js
@@ -9,11 +9,23 @@ export const Product = () => {
           <div className={styles.productInfo}>
             <div className={styles.productTitle}>12-Slat Deck Tile</div>
             <div className={styles.productTagline}>Outdoor Floor Tile, Wooden, Acacia</div>
-            <div className={styles.productDesc}>12” x 12” Square Acacia Hardwood Flooring Wood Tiles, Patio Deck, Paver Interlocking tiles, Easy Install Indoor Outdoor, Pack of 10, Dark Brown. Crafted from high-quality natural wood. Not only are highly functional, minimal yet trendy designs of our products make them great decorations for your houses.</div>
+            <div className={styles.productDesc}>12” x 12” Square Acacia Hardwood Flooring Wood Tiles, Patio Deck, Paver Interlocking tiles, Easy Install Indoor Outdoor, Pack of 10, Dark Brown. Crafted from high-quality natural wood. Not only are highly functional, minimal yet trendy designs of our products make them great decorations for your houses.
+            </div>
+            <div className={styles.readMore}>
+              Read more 
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
           </div>
           <div className={styles.imagePart}>
-            <div className={styles.imageProduct}>
-              <img alt='12-slat deck tiles' src='/products/12-slat.png' style={{height:"500px"}}></img>
+            <div className={styles.imageWrapper}>
+              <div className={styles.productBackground}></div>
+              <div className={styles.imageProduct}>
+                <img alt='12-slat deck tiles' src='/products/12-slat.png' ></img>
+              </div>
+              <div className={styles.button}>
+                <i class="fas fa-shopping-bag"></i> 
+                <span>Add to cart</span>
+              </div>
             </div>
           </div>
         </div>
